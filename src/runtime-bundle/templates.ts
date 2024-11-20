6ceb4133f571701c0352ed2673edc6facb9ebad9
Jorge Lara <jelch2002@gmail.com>
2024-11-20 12:48:10 -0600

    fix: update templates to new btc runtime standard

--- src/runtime-bundle/templates.ts
+++ src/runtime-bundle/templates.ts
@@ -14,49 +14,67 @@ const op20_build = ({
     tokenMaxSupply,
 }: OP20BuildConfig) => ({
     [`contracts/${tokenName}.ts`]: `
-import { u128, u256 } from 'as-bignum/assembly';
 import {
     Address,
     Blockchain,
     BytesWriter,
     Calldata,
+    DeployableOP_20,
     encodeSelector,
     Map,
     OP20InitParameters,
-    OP_20,
     Selector,
 } from '@btc-vision/btc-runtime/runtime';
-import { DeployableOP_20 } from '@btc-vision/btc-runtime/runtime/contracts/DeployableOP_20';
+import { u128, u256 } from 'as-bignum/assembly';
 
 @final
-export class ${tokenName} extends OP_20 {
-    constructor() {
-        const maxSupply: u256 = u128.fromString('${tokenMaxSupply}').toU256();
-        const decimals: u8 = ${tokenDecimals};
-        const name: string = '${tokenName}';
-        const symbol: string = '${tokenSymbol}';
+export class ${tokenName} extends DeployableOP_20 {
+    public constructor() {
+        super();
 
-        super(maxSupply, decimals, name, symbol);
+        // IMPORTANT. THIS WILL RUN EVERYTIME THE CONTRACT IS INTERACTED WITH. FOR SPECIFIC INITIALIZATION, USE "onDeployment" METHOD.
     }
 
-    public onInstantiated(): void {
-        if (!this.isInstantiated) {
-            super.onInstantiated();
+    // "solidityLikeConstructor" This is a solidity-like constructor. This method will only run once when the contract is deployed.
+    public override onDeployment(_calldata: Calldata): void {
+        const maxSupply: u256 = u128.fromString('${tokenMaxSupply}').toU256(); // Your max supply.
+        const decimals: u8 = ${tokenDecimals}; // Your decimals.
+        const name: string = '${tokenName}'; // Your token name.
+        const symbol: string = '${tokenSymbol}'; // Your token symbol.
 
-        }
+        this.instantiate(new OP20InitParameters(maxSupply, decimals, name, symbol));
 
+        // Add your logic here. Eg, minting the initial supply:
+        this._mint(Blockchain.tx.origin, maxSupply);
     }
 
-    public override callMethod(method: Selector, calldata: Calldata): BytesWriter {
+    public override execute(method: Selector, calldata: Calldata): BytesWriter {
         switch (method) {
             case encodeSelector('airdrop'):
                 return this.airdrop(calldata);
+            case encodeSelector('mint'):
+                return this.mint(calldata);
+            case encodeSelector('airdropWithAmount'):
+                return this.airdropWithAmount(calldata);
             default:
-                return super.callMethod(method, calldata);
+                return super.execute(method, calldata);
         }
     }
 
+    private mint(callData: Calldata): BytesWriter {
+        this.onlyOwner(Blockchain.tx.sender);
+
+        const response = new BytesWriter(1);
+        const resp = this._mint(callData.readAddress(), callData.readU256());
+
+        response.writeBoolean(resp);
+
+        return response;
+    }
+
     private airdrop(calldata: Calldata): BytesWriter {
+        this.onlyOwner(Blockchain.tx.sender);
+
         const drops: Map<Address, u256> = calldata.readAddressValueTuple();
 
         const addresses: Address[] = drops.keys();
@@ -64,8 +82,33 @@ export class ${tokenName} extends OP_20 {
             const address = addresses[i];
             const amount = drops.get(address);
 
-            this._mint(address, amount);
+            this._mint(address, amount, false);
         }
+
+        const writer: BytesWriter = new BytesWriter(1);
+        writer.writeBoolean(true);
+
+        return writer;
+    }
+
+    private _optimizedMint(address: Address, amount: u256): void {
+        this.balanceOfMap.set(address, amount);
+        this._totalSupply.addNoCommit(amount);
+
+        this.createMintEvent(address, amount);
+    }
+
+    private airdropWithAmount(calldata: Calldata): BytesWriter {
+        this.onlyOwner(Blockchain.tx.sender);
+
+        const amount: u256 = calldata.readU256();
+        const addresses: Address[] = calldata.readAddressArray();
+
+        for (let i: i32 = 0; i < addresses.length; i++) {
+            this._optimizedMint(addresses[i], amount);
+        }
+
+        this._totalSupply.commit();
 
         const writer: BytesWriter = new BytesWriter(1);
         writer.writeBoolean(true);
@@ -75,37 +118,15 @@ export class ${tokenName} extends OP_20 {
 }
 `,
     'index.ts': `
-import { ABIRegistry, Blockchain } from '@btc-vision/btc-runtime/runtime';
-import { ${tokenName} } from './contracts/${tokenName}';
-
-export function defineSelectors(): void {
-    /** OP_NET */
-    ABIRegistry.defineMethodSelector('isAddressOwner');
-
-    /** OP_20 */
-    ABIRegistry.defineMethodSelector('allowance');
-    ABIRegistry.defineMethodSelector('approve');
-    ABIRegistry.defineMethodSelector('balanceOf');
-    ABIRegistry.defineMethodSelector('burn');
-    ABIRegistry.defineMethodSelector('mint');
-    ABIRegistry.defineMethodSelector('transfer');
-    ABIRegistry.defineMethodSelector('transferFrom');
-
-    /** Optional */
-    ABIRegistry.defineMethodSelector('airdrop');
-
-    // Define your selectors here.
-}
+import { Blockchain } from '@btc-vision/btc-runtime/runtime';
+import { MyToken } from './contracts/MyToken';
 
 // DO NOT TOUCH TO THIS.
 Blockchain.contract = () => {
     // ONLY CHANGE THE CONTRACT CLASS NAME.
-    const contract = new ${tokenName}();
-    contract.onInstantiated();
-
     // DO NOT ADD CUSTOM LOGIC HERE.
 
-    return contract;
+    return new MyToken();
 };
 
 // VERY IMPORTANT
