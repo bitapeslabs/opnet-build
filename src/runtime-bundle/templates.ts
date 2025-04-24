a24295051900ddd764c6aa7b1f6dd9ff9748ff19
Jorge Lara <jelch2002@gmail.com>
2025-04-24 16:58:01 -0600

    (bot) automated build - update to @btc-vision/btc-runtime^1.5.8

--- src/runtime-bundle/templates.ts
+++ src/runtime-bundle/templates.ts
@@ -14,21 +14,20 @@ const op20_build = ({
     tokenMaxSupply,
 }: OP20BuildConfig) => ({
     [`contracts/${tokenName}.ts`]: `
+mport { u256 } from '@btc-vision/as-bignum/assembly';
 import {
     Address,
+    AddressMap,
     Blockchain,
+    BOOLEAN_BYTE_LENGTH,
     BytesWriter,
     Calldata,
     DeployableOP_20,
-    encodeSelector,
-    Map,
     OP20InitParameters,
-    Selector,
 } from '@btc-vision/btc-runtime/runtime';
-import { u128, u256 } from 'as-bignum/assembly';
 
 @final
-export class ${tokenName} extends DeployableOP_20 {
+export class MyToken extends DeployableOP_20 {
     public constructor() {
         super();
 
@@ -37,7 +36,7 @@ export class ${tokenName} extends DeployableOP_20 {
 
     // "solidityLikeConstructor" This is a solidity-like constructor. This method will only run once when the contract is deployed.
     public override onDeployment(_calldata: Calldata): void {
-        const maxSupply: u256 = u128.fromString('${tokenMaxSupply}').toU256(); // Your max supply.
+        const maxSupply: u256 = u256.fromString('${tokenMaxSupply}'); // Your max supply.
         const decimals: u8 = ${tokenDecimals}; // Your decimals.
         const name: string = '${tokenName}'; // Your token name.
         const symbol: string = '${tokenSymbol}'; // Your token symbol.
@@ -45,37 +44,48 @@ export class ${tokenName} extends DeployableOP_20 {
         this.instantiate(new OP20InitParameters(maxSupply, decimals, name, symbol));
 
         // Add your logic here. Eg, minting the initial supply:
-        this._mint(Blockchain.tx.origin, maxSupply);
+        // this._mint(Blockchain.tx.origin, maxSupply);
     }
 
-    public override execute(method: Selector, calldata: Calldata): BytesWriter {
-        switch (method) {
-            case encodeSelector('airdrop'):
-                return this.airdrop(calldata);
-            case encodeSelector('mint'):
-                return this.mint(calldata);
-            case encodeSelector('airdropWithAmount'):
-                return this.airdropWithAmount(calldata);
-            default:
-                return super.execute(method, calldata);
-        }
-    }
+    @method(
+        {
+            name: 'address',
+            type: ABIDataTypes.ADDRESS,
+        },
+        {
+            name: 'amount',
+            type: ABIDataTypes.UINT256,
+        },
+    )
+    @returns({
+        name: 'success',
+        type: ABIDataTypes.BOOL,
+    })
+    @emit('Transfer')
+    public mint(calldata: Calldata): BytesWriter {
+        this.onlyDeployer(Blockchain.tx.sender);
 
-    private mint(callData: Calldata): BytesWriter {
-        this.onlyOwner(Blockchain.tx.sender);
-
-        const response = new BytesWriter(1);
-        const resp = this._mint(callData.readAddress(), callData.readU256());
+        const response = new BytesWriter(BOOLEAN_BYTE_LENGTH);
+        const resp = this._mint(calldata.readAddress(), calldata.readU256());
 
         response.writeBoolean(resp);
 
         return response;
     }
 
-    private airdrop(calldata: Calldata): BytesWriter {
-        this.onlyOwner(Blockchain.tx.sender);
+    @method({
+        name: 'drops',
+        type: ABIDataTypes.ADDRESS_UINT256_TUPLE,
+    })
+    @returns({
+        name: 'success',
+        type: ABIDataTypes.BOOL,
+    })
+    @emit('Transfer')
+    public airdrop(calldata: Calldata): BytesWriter {
+        this.onlyDeployer(Blockchain.tx.sender);
 
-        const drops: Map<Address, u256> = calldata.readAddressValueTuple();
+        const drops: AddressMap<u256> = calldata.readAddressMapU256();
 
         const addresses: Address[] = drops.keys();
         for (let i: i32 = 0; i < addresses.length; i++) {
@@ -85,7 +95,40 @@ export class ${tokenName} extends DeployableOP_20 {
             this._mint(address, amount, false);
         }
 
-        const writer: BytesWriter = new BytesWriter(1);
+        const writer: BytesWriter = new BytesWriter(BOOLEAN_BYTE_LENGTH);
+        writer.writeBoolean(true);
+
+        return writer;
+    }
+
+    @method(
+        {
+            name: 'amount',
+            type: ABIDataTypes.UINT256,
+        },
+        {
+            name: 'addresses',
+            type: ABIDataTypes.ARRAY_OF_ADDRESSES,
+        },
+    )
+    @returns({
+        name: 'success',
+        type: ABIDataTypes.BOOL,
+    })
+    @emit('Transfer')
+    public airdropWithAmount(calldata: Calldata): BytesWriter {
+        this.onlyDeployer(Blockchain.tx.sender);
+
+        const amount: u256 = calldata.readU256();
+        const addresses: Address[] = calldata.readAddressArray();
+
+        for (let i: i32 = 0; i < addresses.length; i++) {
+            this._optimizedMint(addresses[i], amount);
+        }
+
+        this._totalSupply.commit();
+
+        const writer: BytesWriter = new BytesWriter(BOOLEAN_BYTE_LENGTH);
         writer.writeBoolean(true);
 
         return writer;
@@ -97,40 +140,26 @@ export class ${tokenName} extends DeployableOP_20 {
 
         this.createMintEvent(address, amount);
     }
+}`,
 
-    private airdropWithAmount(calldata: Calldata): BytesWriter {
-        this.onlyOwner(Blockchain.tx.sender);
-
-        const amount: u256 = calldata.readU256();
-        const addresses: Address[] = calldata.readAddressArray();
-
-        for (let i: i32 = 0; i < addresses.length; i++) {
-            this._optimizedMint(addresses[i], amount);
-        }
-
-        this._totalSupply.commit();
-
-        const writer: BytesWriter = new BytesWriter(1);
-        writer.writeBoolean(true);
-
-        return writer;
-    }
-}
-`,
-    'index.ts': `
-import { Blockchain } from '@btc-vision/btc-runtime/runtime';
-import { MyToken } from './contracts/MyToken';
+    ['index.ts']: `import { Blockchain } from '@btc-vision/btc-runtime/runtime';
+import { ${tokenName} } from './contracts/${tokenName}';
+import { revertOnError } from '@btc-vision/btc-runtime/runtime/abort/abort';
 
 // DO NOT TOUCH TO THIS.
 Blockchain.contract = () => {
     // ONLY CHANGE THE CONTRACT CLASS NAME.
     // DO NOT ADD CUSTOM LOGIC HERE.
 
-    return new MyToken();
+    return new ${tokenName}();
 };
 
 // VERY IMPORTANT
 export * from '@btc-vision/btc-runtime/runtime/exports';
-`,
+
+// VERY IMPORTANT
+export function abort(message: string, fileName: string, line: u32, column: u32): void {
+    revertOnError(message, fileName, line, column);
+}`,
 });
 export { op20_build };
